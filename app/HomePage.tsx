c2f7bd0829bf20afb524c86c3c97a4ad736d1364
Samuel <Samkostofik@gmail.com>
2025-05-11 08:54:56 +0200

    App initialized

---
+++ app/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+    <View className="h-full bg-slate-700">
+      <Text>Hello</Text>
+    </View>
+    </>
+  )
+}
+export default HomePage
